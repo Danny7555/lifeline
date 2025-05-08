0f775ca9724c4556fb19541cc969d0ae7c1d7a2b
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2025-05-08 19:23:51 +0000

    fixed the style components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import SignIn from "./[auth]/signIn/page";
 
 export default function page() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <>
       <SignIn />
-    </main>
+    </>
   )
 }
