6ef53a32adaf98ced95058feff96b21cbc1d6ede
Aristocratjnr <daobuobi006@st.ug.edu.gh>
2025-06-01 10:04:09 +0000

    fix(profile): adjust margin for improved layout in ProfileContent component

--- src/app/dashboard/profile/ProfileContent.tsx
+++ src/app/dashboard/profile/ProfileContent.tsx
@@ -271,7 +271,7 @@ export default function ProfileContent() {
                   Location: {profileData.location || "Not set"}
                 </p>
               </div>
-              <div className="mt-2 sm:mt-0">
+              <div className="-mt-2 sm:mt-0">
                 <p className="text-black text-sm sm:text-base flex items-center justify-center sm:justify-start gap-2">
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="#666666" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                     <path d="M20.42 4.58a5.4 5.4 0 0 0-7.65 0l-.77.78-.77-.78a5.4 5.4 0 0 0-7.65 0C1.46 6.7 1.33 10.28 4 13l8 8 8-8c2.67-2.72 2.54-6.3.42-8.42z"></path>
